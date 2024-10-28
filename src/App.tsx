66bc0aa94491dd86ed797cd1253f37b6105e9cf7
Arnaldas Augutis <arnaldas@taal.com>
2024-10-28 11:42:53 +0200

    working version

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   fromUtxo,
   P2PKH,
   PrivateKey,
   PublicKey,
+  Script,
   Transaction,
   TransactionOutput,
   UnlockingScript,
+  Utils,
 } from '@bsv/sdk';
 import { SignatureRequest, useYoursWallet } from 'yours-wallet-provider';
 
@@ -15,10 +17,25 @@ import './App.css';
 function App() {
   const wallet = useYoursWallet();
 
+  useEffect(() => {
+    if (!wallet?.on) return;
+    wallet.on('switchAccount', () => {
+      console.log('switchAccount');
+      // handle switching or creating a new account for the user within your app
+    });
+
+    wallet.on('signedOut', () => {
+      console.log('signedOut');
+      wallet.disconnect();
+    });
+  }, [wallet]);
+
   const test = async () => {
     if (!wallet?.isReady) {
       throw new Error('Wallet is not ready');
     }
+
+    await wallet.connect();
 
     const utxoList = await wallet.getPaymentUtxos();
     const pubKeys = await wallet.getPubKeys();
@@ -26,13 +43,15 @@ function App() {
     const addressList = await wallet.getAddresses();
 
     if (!pubKeys?.identityPubKey || !utxoList?.length || !addressList?.identityAddress) {
+      console.error({ pubKeys, utxoList, addressList });
       throw new Error('Something went wrong');
     }
 
-    const pubKey = PublicKey.fromString(pubKeys.identityPubKey);
+    const addressToUseInSignatureRequest = Utils.toBase58Check(
+      Script.fromHex(utxoList[0].script).chunks[2].data as number[]
+    );
 
-    // const addr = pubKey.toAddress([0x6f]);
-    // const addr = pubKey.toAddress();
+    const pubKey = PublicKey.fromString(pubKeys.identityPubKey);
     const { identityAddress: addr, bsvAddress: bsvAddr } = addressList!;
 
     console.log({
@@ -78,9 +97,7 @@ function App() {
         outputIndex: t2.inputs[0].sourceOutputIndex,
         inputIndex: 0,
         satoshis: t2.inputs[0].sourceTransaction!.outputs[t2.inputs[0].sourceOutputIndex].satoshis!,
-        address: addr,
-        // sigHashType: 0x01,
-        // sigHashType: 65,
+        address: addressToUseInSignatureRequest,
       },
     ];
 
@@ -94,7 +111,11 @@ function App() {
 
     console.log({ sigResponses });
 
-    t2.inputs[0].unlockingScript = UnlockingScript.fromHex(sigResponses![0].sig);
+    const u = new UnlockingScript();
+    u.writeBin(Utils.toArray(sigResponses?.[0]?.sig, 'hex'));
+    u.writeBin(Utils.toArray(sigResponses?.[0]?.pubKey, 'hex'));
+
+    t2.inputs[0].unlockingScript = u;
 
     console.log('t2', t2.toHex());
   };
